1b8840272bc08245f21df6e8ef29eb344bdc19db
Antonina Komarova <Anthonina@yandex.ru>
2018-04-08 23:58:26 +0300

    Add files via upload

--- labirint_parser.py
+++ labirint_parser.py
@@ -1,10 +1,18 @@
 import requests
+import time
+import json
 from bs4 import BeautifulSoup
 
 
 def get_html(url):
     response_url = requests.get(url)
     return response_url.text
+
+def clear_string(in_string, find = ['Масса:', 'Размеры:', 'Страниц:', 'Издательство:', 'ISBN:', 'Цена', 'ID товара:']):
+    for f in find:
+        in_string = in_string.replace(f, '').strip()
+
+    return in_string
 
 
 def get_page_data(html):
@@ -48,7 +56,7 @@ def get_page_data(html):
 
     for data in product_data:
         attr_name = data['class'][0]
-        goods_data[attr_name] = data.text.replace('\n', ' ')
+        goods_data[attr_name] = clear_string(data.text.replace('\n', ' '))
 
 
     try:
@@ -69,11 +77,41 @@ def get_page_data(html):
 
 
 def main():
-    url = 'https://www.labirint.ru/office/635306/'
-    html = get_html (url)
-    page_data = get_page_data(html)
 
-    print(page_data)
+    working_file_json = 'labirint_json.txt'
+    file_labirint_json = open(working_file_json, mode = 'w', encoding = 'utf8')
+
+    #labirint_url_array = ['https://www.labirint.ru/office/10002/', 'https://www.labirint.ru/office/10009/']
+    with open('labirint_url.txt') as file:
+        labirint_url_array = [row.strip() for row in file]
+
+    labirint_array_json = []
+
+    index = 0
+    length = len(labirint_url_array)
+    for url_labirint in labirint_url_array:
+        html = get_html (url_labirint)
+        page_data = get_page_data(html)
+        labirint_array_json.append(page_data)
+        index += 1
+        print("Parsed {} of {} pages".format(index, length))
+
+        if index == 10:
+            break
+
+        if index < length:
+            time.sleep(3)
+        
+    json.dump(labirint_array_json, file_labirint_json, ensure_ascii=False)
+    
+    file_labirint_json.close()
+
+    print("Parsing complete!")
+
+
+        #print(page_data)
+        #print('--------------------')
+        
 
 if __name__ == '__main__':
     main()
